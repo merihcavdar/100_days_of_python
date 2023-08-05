5b64de3c10f6c2726c44e99255dc7100c3f28378
Merih <cavdarmerih@hotmail.com>
2023-08-05 18:11:45 +0300

    day 2 partial

---
+++ day2_data_types.py
@@ -0,0 +1,21 @@
+# data types
+
+# string
+
+"Hello"[0] # subscripting
+
+print("123" + "456") # concatenate
+
+# integer
+
+print(123+456)
+
+123_456_789
+
+# float
+3.14159
+
+# boolean
+
+True
+False
