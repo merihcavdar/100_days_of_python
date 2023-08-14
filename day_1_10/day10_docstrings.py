d299d4510b4e87f692f95fd22433853b3462e3ed
Merih <cavdarmerih@hotmail.com>
2023-08-14 23:53:45 +0300

    day 9 complete

---
+++ day_1_10/day10_docstrings.py
@@ -0,0 +1,13 @@
+# used for documenting
+# when you use a function and open prenthesis, it gives you hint about the parameters.
+
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "you didn't provide valid inputs."
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"Result: {formatted_f_name} {formatted_l_name}"
+
+
+format_name()
---
+++ day_1_10/day10_functions_with_outputs.py
@@ -0,0 +1,9 @@
+def my_function():
+    result = 3*2
+    return result
+
+
+def format_name(f_name, l_name):
+    return str(f_name).title() + " " + str(l_name).title()
+
+print(format_name("merih", "çavdar"))
