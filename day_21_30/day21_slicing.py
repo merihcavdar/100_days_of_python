1aa76879a35582c27fc96248dd142031d206c931
merihcavdar <cavdarmerih@hotmail.com>
2023-09-06 15:09:43 +0300

    day 21 complete

---
+++ day_21_30/day21_slicing.py
@@ -0,0 +1,12 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+
+print(piano_keys[2:5]) # between 2 and 5
+print(piano_keys[2:]) # from 2
+print(piano_keys[:5]) #up to 5
+print(piano_keys[2:5:2]) # third parameter for increment
+print(piano_keys[::2]) # every secnod item
+print(piano_keys[::-1]) # reverse the list
+
+piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
+
+print(piano_tuple[2:5])
