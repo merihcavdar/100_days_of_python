d54c7dc96b97d05379ac318a5871ba76fc81ecf9
merihcavdar <cavdarmerih@hotmail.com>
2023-10-09 18:25:30 +0300

    day 34 complete

---
+++ day_21_30/day34/python_typing.py
@@ -0,0 +1,20 @@
+#age: int
+name: str
+height: float
+is_human: bool
+
+
+def police_check(age: int) -> bool: # type hints
+    if age>18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+
+if police_check(19):
+    print("you may pass")
+else:
+    print("pay a fine")
+
+
